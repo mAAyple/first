1e92a46c6f24368371173199368d3c3d99f3d33e
mAyple <benjaminesher@gmail.com>
2015-11-19 22:23:31 +0000

    fuck u oli

---
+++ roboticsnoodling/noodle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+
+    return 0;
+}
+
+
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
+//remember use gcc to complile, and dir to find your location
